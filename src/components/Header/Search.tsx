bf1445deb31897437af5f33337eeb763d4990290
Ivan <adamovskij00@mail.ru>
2022-03-21 22:42:28 +0300

    add Search styles

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,10 +1,13 @@
+import {AiOutlineSearch} from "react-icons/ai";
+
 export const Search = () => {
   return (
-    <div className="bg-white">
+    <div className="flax relative items-center w-1/2 px-4 py-2 ml-2 bg-gray-200 rounded-full">
+      <AiOutlineSearch className='absolute left-2 bottom-2.5 text-lg text-gray-600'/>
       <input
         type="text"
         placeholder="Search Facebook"
-        className="rounded-full"
+        className="w-auto px-2 ml-2 bg-gray-200"
       />
     </div>
   )
