aac20366d6d26e4c9e7eca99018fbbb4911d8c7d
Ivan <adamovskij00@mail.ru>
2022-03-21 22:41:19 +0300

    add Copyright component

---
+++ src/components/LeftSide/Copyright.tsx
@@ -0,0 +1,29 @@
+export const Copyright = () => {
+  return (
+    <div>
+      <ul className="list-none flex flex-wrap">
+        <li className="left-sidebar__list-item">
+          <a className="text-gray-400 text-xs" href="/">Privacy</a>
+        </li>
+        <li className="left-sidebar__list-item">
+          <a className="text-gray-400 text-xs" href="/">Terms</a>
+        </li>
+        <li className="left-sidebar__list-item">
+          <a className="text-gray-400 text-xs" href="/">Advertising</a>
+        </li>
+        <li className="left-sidebar__list-item">
+          <a className="text-gray-400 text-xs" href="/">Add Choices</a>
+        </li>
+        <li className="left-sidebar__list-item">
+          <a className="text-gray-400 text-xs" href="/">Cookies</a>
+        </li>
+        <li className="left-sidebar__list-item">
+          <a className="text-gray-400 text-xs" href="/">More</a>
+        </li>
+        <li className="left-sidebar__list-item">
+          <a className="text-gray-400 text-xs" href="/">Facebook &#169; 2022</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
