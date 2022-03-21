92b4965b3c8305560bc3d9ba3809a38e9946d1ff
Ivan <adamovskij00@mail.ru>
2022-03-21 22:42:17 +0300

    add Profile block

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,7 +1,9 @@
+import {AiOutlineSetting} from "react-icons/ai";
+
 export const Profile = () => {
   return (
-    <div>
-      Profile
+    <div className="flex items-center justify-end">
+      <AiOutlineSetting className="text-3xl hover:text-gray-500"/>
     </div>
   )
 }
