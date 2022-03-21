a0a387fbead2da4684b2aa5e9a4c632014c5b890
Ivan <adamovskij00@mail.ru>
2022-03-21 22:41:28 +0300

    add Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,21 +4,21 @@ import {Profile} from "./Profile";
 
 export const Header = () => {
   return (
-    <section className="p-3 flex items-center justify-between">
-      <div className="w-1/4">
+    <section className="flex px-4 py-2 items-center justify-between shadow">
+      <div className="flex w-1/3">
         <div className='logo'>
           <img
-            className="h-7 rounded-full"
+            className="h-9 rounded-full"
             src="https://cdn.svgporn.com/logos/facebook.svg"
             alt='logo'
           />
         </div>
         <Search/>
       </div>
-      <div className='w-1/2'>
+      <div className='w-1/3'>
         <Menu/>
       </div>
-      <div className='w-1/4'>
+      <div className='w-1/3'>
         <Profile/>
       </div>
     </section>
