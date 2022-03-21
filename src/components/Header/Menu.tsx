eef659f54a6127034cdf97329c8813466fe9b643
Ivan <adamovskij00@mail.ru>
2022-03-21 22:41:03 +0300

    add icons

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -1,7 +1,64 @@
+import {AiOutlineHome} from "react-icons/ai";
+import {AiOutlineNotification} from "react-icons/ai";
+import {AiOutlinePlaySquare} from "react-icons/ai";
+import {AiOutlineShopping} from "react-icons/ai";
+import {AiOutlineTeam} from "react-icons/ai";
+import {AiOutlineMessage} from "react-icons/ai";
+import avatar from "./../../avatar.jpg";
+
+export const Avatar = () => {
+  return (
+    <div>
+      <img
+        className='w-7 h-7 m-auto rounded-full overflow-hidden'
+        src={avatar}
+        width='30'
+        alt="avatar"
+      />
+    </div>
+  )
+}
+
 export const Menu = () => {
   return (
     <nav>
-      Menu navigation
+      <ul className="list-none flex justify-between items-center menu">
+        <li className="nav-item nav-item__active">
+          <a className="nav-link" href="#">
+            <AiOutlineHome className="nav-img"/>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="">
+            <AiOutlineNotification className="nav-img"/>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <AiOutlinePlaySquare className="nav-img"/>
+          </a>
+        </li>
+        <li >
+          <a className="nav-link" href="#">
+            <Avatar/>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <AiOutlineShopping className="nav-img"/>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <AiOutlineTeam className="nav-img"/>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            <AiOutlineMessage className="nav-img"/>
+          </a>
+        </li>
+      </ul>
     </nav>
   )
 }
