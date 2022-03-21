8a58050e25b59c2554612d4172f1be62e03e1b9f
Ivan <adamovskij00@mail.ru>
2022-03-21 20:30:18 +0300

    add Header sections

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,26 @@
+import {Menu} from "./Menu";
+import {Search} from "./Search";
+import {Profile} from "./Profile";
+
 export const Header = () => {
   return (
-    <section>
-      Header
+    <section className="p-3 flex items-center justify-between">
+      <div className="w-1/4">
+        <div className='logo'>
+          <img
+            className="h-7 rounded-full"
+            src="https://cdn.svgporn.com/logos/facebook.svg"
+            alt='logo'
+          />
+        </div>
+        <Search/>
+      </div>
+      <div className='w-1/2'>
+        <Menu/>
+      </div>
+      <div className='w-1/4'>
+        <Profile/>
+      </div>
     </section>
   )
 }
