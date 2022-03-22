b422970360ab9fa6d4b538d5d6aa388ae80f4dec
Ivan <adamovskij00@mail.ru>
2022-03-22 11:37:33 +0300

    add upd Profile btn styles

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -3,7 +3,9 @@ import {AiOutlineSetting} from "react-icons/ai";
 export const Profile = () => {
   return (
     <div className="flex items-center justify-end">
-      <AiOutlineSetting className="text-3xl hover:text-gray-500"/>
+      <div className="p-1 bg-gray-200 rounded-full">
+        <AiOutlineSetting className="text-3xl cursor-pointer hover:text-gray-500"/>
+      </div>
     </div>
   )
 }
