d46a770fea2bdccf2b87dd8c06f8398735670859
Ivan <adamovskij00@mail.ru>
2022-03-22 09:39:01 +0300

    add LeftSidebar refactoring

--- src/components/LeftSide/LeftSidebar.tsx
+++ src/components/LeftSide/LeftSidebar.tsx
@@ -7,6 +7,42 @@ import drive from "./../../img/drive.png";
 import health from "./../../img/health.png";
 import settings from "./../../img/settings.png";
 
+type MenuItemType = {
+  name: string,
+  image: any
+}
+
+const menuList = [
+  {
+    name: 'Events',
+    image: calendar
+  },
+  {
+    name: 'Saved',
+    image: drive
+  },
+  {
+    name: 'Gaming',
+    image: discord
+  },
+  {
+    name: 'Fundraisers',
+    image: exchange
+  },
+  {
+    name: 'Memories',
+    image: app
+  },
+  {
+    name: 'Help & Support',
+    image: health
+  },
+  {
+    name: 'Settings & Privacy',
+    image: settings
+  }
+] as Array<MenuItemType>;
+
 export const LeftSidebar = () => {
   return (
     <section className="shadow-lg p-4 w-1/4 ">
@@ -15,48 +51,16 @@ export const LeftSidebar = () => {
         <a href="/" className='text-blue-400'>Create</a>
       </div>
       <ul className="list-none">
-        <li>
-          <a className="flex items-center py-2" href="/">
-            <img src={calendar} alt="" className="h-9 w-9"/>
-            <span className="pl-3"> Events</span>
-          </a>
-        </li>
-        <li>
-          <a className="flex items-center py-2" href="/">
-            <img src={drive} alt="" className="h-9 w-9"/>
-            <span className="pl-3">Saved</span>
-          </a>
-        </li>
-        <li>
-          <a className="flex items-center py-2" href="/">
-            <img src={discord} alt="" className="h-9 w-9"/>
-            <span className="pl-3">Gaming</span>
-          </a>
-        </li>
-        <li>
-          <a className="flex items-center py-2" href="/">
-            <img src={exchange} alt="" className="h-9 w-9"/>
-            <span className="pl-3">Fundraisers</span>
-          </a>
-        </li>
-        <li>
-          <a className="flex items-center py-2" href="/">
-            <img src={app} alt="" className="h-9 w-9"/>
-            <span className="pl-3">Memories</span>
-          </a>
-        </li>
-        <li>
-          <a className="flex items-center py-2" href="/">
-            <img src={health} alt="health" className="h-9 w-9"/>
-            <span className="pl-3">Help & Support</span>
-          </a>
-        </li>
-        <li>
-          <a className="flex items-center py-2" href="/">
-            <img src={settings} alt="settings" className="h-9 w-9"/>
-            <span className="pl-3">Settings & Privacy</span>
-          </a>
-        </li>
+        {
+          menuList.map(item => (
+            <li>
+              <a className="flex items-center py-2" href="/">
+                <img src={item.image} alt="" className="h-9 w-9"/>
+                <span className="pl-3">{item.name}</span>
+              </a>
+            </li>
+          ))
+        }
       </ul>
       <Copyright/>
     </section>
