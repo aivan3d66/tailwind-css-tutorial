ae69ba77c3806635b5541fc1453293a38af8a9e1
Ivan <adamovskij00@mail.ru>
2022-03-22 10:14:59 +0300

    add upd RightSidebar styles

--- src/components/RightSide/RightSidebar.tsx
+++ src/components/RightSide/RightSidebar.tsx
@@ -1,3 +1,5 @@
+import {AiOutlineMenu} from "react-icons/ai";
+
 type ContactType = {
   name: string,
   avatar: string
@@ -32,15 +34,15 @@ const contacts = [
 
 export const RightSidebar = () => {
   return (
-    <section>
+    <section className="shadow-lg p-4 w-1/4 h-screen">
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-bold">Contacts</h2>
-        <a href="/" className='text-blue-400'>Create</a>
+        <AiOutlineMenu className="text-gray-600"/>
       </div>
       <ul className="list-none">
         {
-          contacts.map(item => (
-            <li>
+          contacts.map((item, index) => (
+            <li key={index}>
               <a className="flex items-center py-2" href="/">
                 <div className="h-9 w-9 rounded-full overflow-hidden">
                   <img src={item.avatar} alt="user avatar"/>
