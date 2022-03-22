a63def5cd17f261fefc7a05d14009565c450a6c4
Ivan <adamovskij00@mail.ru>
2022-03-22 10:14:11 +0300

    add upd LeftSidebar styles

--- src/components/LeftSide/LeftSidebar.tsx
+++ src/components/LeftSide/LeftSidebar.tsx
@@ -45,24 +45,26 @@ const menuList = [
 
 export const LeftSidebar = () => {
   return (
-    <section className="shadow-lg p-4 w-1/4 ">
+    <section className="shadow-lg p-4 w-1/4 h-screen">
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-bold">Home</h2>
         <a href="/" className='text-blue-400'>Create</a>
       </div>
-      <ul className="list-none">
-        {
-          menuList.map(item => (
-            <li>
-              <a className="flex items-center py-2" href="/">
-                <img src={item.image} alt="" className="h-9 w-9"/>
-                <span className="pl-3">{item.name}</span>
-              </a>
-            </li>
-          ))
-        }
-      </ul>
-      <Copyright/>
+      <div className="flex flex-col justify-between">
+        <ul className="list-none mb-20">
+          {
+            menuList.map((item, index) => (
+              <li key={index}>
+                <a className="flex items-center py-2" href="/">
+                  <img src={item.image} alt="" className="h-9 w-9"/>
+                  <span className="pl-3">{item.name}</span>
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+        <Copyright/>
+      </div>
     </section>
   )
 }
