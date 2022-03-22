0797783cbd7957f01c9cf2c732df9c700a4e3b08
Ivan <adamovskij00@mail.ru>
2022-03-22 11:25:26 +0300

    add Stories import

--- src/components/Center/CenterContent.tsx
+++ src/components/Center/CenterContent.tsx
@@ -1,9 +1,11 @@
 import {AddPost} from "./AddPost";
 import {Post} from "./Post";
+import {Stories} from "./Stories/Stories";
 
 export const CenterContent = () => {
   return (
-    <section className="w-1/2 mt-10">
+    <section className="w-1/2 mt-1">
+      <Stories/>
       <AddPost/>
       <Post/>
     </section>
