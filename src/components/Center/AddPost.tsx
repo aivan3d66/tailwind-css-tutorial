6190789c1c2532106e006ae99d9c0a88c79cfee2
Ivan <adamovskij00@mail.ru>
2022-03-22 09:38:36 +0300

    add upd AddPost component (add styles, content)

--- src/components/Center/AddPost.tsx
+++ src/components/Center/AddPost.tsx
@@ -1,10 +1,12 @@
 import {Avatar} from "../Header/Menu";
+import {AiOutlineFileImage} from "react-icons/ai";
 
 export const AddPost = () => {
   return (
-    <div>
+    <div className='flex items-center shadow-lg rounded'>
       <Avatar/>
-      <input type="text"/>
+      <button className='p-2 w-10/12 rounded sm:bg-gray-200 text-gray-800'>Add post</button>
+      <AiOutlineFileImage className='text-3xl'/>
     </div>
   )
 }
