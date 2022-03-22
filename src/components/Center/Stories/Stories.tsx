3904dc6580c869d1823926a545e377a957de4490
Ivan <adamovskij00@mail.ru>
2022-03-22 11:26:40 +0300

    add Stories component

---
+++ src/components/Center/Stories/Stories.tsx
@@ -0,0 +1,25 @@
+import {users} from "../../../data/users/users";
+import {Story} from "./Story/Story";
+
+export const Stories = () => {
+  return (
+    <div className="p-4">
+      <div className="flex items-center justify-between mb-2">
+        <h2 className="text-xl font-bold">Stories</h2>
+        <a href="/" className='text-blue-400'>See all</a>
+      </div>
+      <div className="flex justify-between flex-row mb-5">
+        {
+          users.map((c, i) => {
+            return <Story
+              key={i}
+              image={c.avatar}
+              name={c.name}
+              photo={c.photo}
+            />
+          })
+        }
+      </div>
+    </div>
+  )
+}
