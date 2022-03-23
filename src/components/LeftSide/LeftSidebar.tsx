9d1c308cbde75a8f81d360df2f61ef05dd1ded76
Ivan <adamovskij00@mail.ru>
2022-03-23 10:27:01 +0300

    add new LeftSidebar width value

--- src/components/LeftSide/LeftSidebar.tsx
+++ src/components/LeftSide/LeftSidebar.tsx
@@ -16,33 +16,31 @@ const container = {
 
 export const LeftSidebar = () => {
   return (
-    <section className="shadow-lg p-4 w-1/4 h-screen">
-      <div>
-        <div className="flex items-center justify-between">
-          <h2 className="text-xl font-bold">Home</h2>
-          <a href="/" className='text-blue-400'>Create</a>
-        </div>
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="flex flex-col justify-between opacity-0"
-        >
-          <ul className="list-none mb-36">
-            {
-              menuList.map((item, index) => (
-                <li key={index} className="left-sidebar__list">
-                  <a className="flex items-center py-2" href="/">
-                    <img src={item.image} alt="" className="h-9 w-9"/>
-                    <span className="pl-3">{item.name}</span>
-                  </a>
-                </li>
-              ))
-            }
-          </ul>
-          <Copyright/>
-        </motion.div>
+    <section className="shadow-lg p-4 sm:w-56 lg:w-1/4  h-screen">
+      <div className="flex items-center justify-between">
+        <h2 className="text-xl font-bold">Home</h2>
+        <a href="/" className='text-blue-400'>Create</a>
       </div>
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        className="flex flex-col justify-between opacity-0"
+      >
+        <ul className="list-none mb-36">
+          {
+            menuList.map((item, index) => (
+              <li key={index} className="left-sidebar__list">
+                <a className="flex items-center py-2" href="/">
+                  <img src={item.image} alt="" className="h-9 w-9"/>
+                  <span className="pl-3">{item.name}</span>
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+        <Copyright/>
+      </motion.div>
     </section>
   )
 }
