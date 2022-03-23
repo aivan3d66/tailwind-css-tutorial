1432d8bc2ab019e38c30f16c6fb211f71f08aeea
Ivan <adamovskij00@mail.ru>
2022-03-23 10:42:23 +0300

    delete div

--- src/components/Center/Stories/Story/Story.tsx
+++ src/components/Center/Stories/Story/Story.tsx
@@ -16,14 +16,12 @@ export const Story: React.FC<StoryType> = ({name, image, photo}) => {
       }}
       className="flex flex-col justify-between p-2 w-36 h-56 min-h-96 rounded hover:bg-contain border-2 cursor-pointer"
     >
-      <div>
-        <div className="h-9 w-9 rounded-full overflow-hidden border-2 border-amber-50">
-          <img src={image} alt="user img"/>
-        </div>
-        <p className="mb-2 text-white text-ml">
-          {name}
-        </p>
+      <div className="h-9 w-9 rounded-full overflow-hidden border-2 border-amber-50">
+        <img src={image} alt="user img"/>
       </div>
+      <p className="mb-2 text-white text-ml">
+        {name}
+      </p>
     </div>
   )
 }
