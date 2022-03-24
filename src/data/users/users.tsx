5589f761cbc715d4395ec25396000c44a8b1487d
Ivan <adamovskij00@mail.ru>
2022-03-24 09:53:20 +0300

    add new user

--- src/data/users/users.tsx
+++ src/data/users/users.tsx
@@ -32,7 +32,11 @@ export const contacts = [
   {
     name: 'Tony Stark',
     avatar: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-  }
+  },
+  {
+    name: 'Natasha Romanov',
+    avatar: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+  },
 ] as Array<ContactType>;
 
 export const users = [
