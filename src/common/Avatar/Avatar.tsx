8398d3da91b182b45881d8a5ea6a1fc6d53b34c5
Ivan <adamovskij00@mail.ru>
2022-03-25 23:03:28 +0300

    add upd Avatar sizes

--- src/common/Avatar/Avatar.tsx
+++ src/common/Avatar/Avatar.tsx
@@ -2,9 +2,9 @@ import avatar from "../../avatar.jpg";
 
 export const Avatar = () => {
   return (
-    <div>
+    <div className="w-7 h-7">
       <img
-        className='w-7 h-7 m-auto rounded-full overflow-hidden'
+        className='w-7 h-7 m-auto rounded-full text-3xl'
         src={avatar}
         width='30'
         alt="avatar"
