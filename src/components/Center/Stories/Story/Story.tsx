1db352868e20bf987ecebfd69f5cb64d46b39cef
Ivan <adamovskij00@mail.ru>
2022-03-25 23:15:05 +0300

    add upd story border rounded

--- src/components/Center/Stories/Story/Story.tsx
+++ src/components/Center/Stories/Story/Story.tsx
@@ -14,7 +14,7 @@ export const Story: React.FC<StoryType> = ({name, image, photo}) => {
         backgroundPosition: "center",
         backgroundSize: "cover"
       }}
-      className="flex flex-col justify-between p-2 w-36 h-56 min-h-96 rounded hover:bg-contain border-2 cursor-pointer"
+      className="flex flex-col justify-between p-2 w-36 h-56 min-h-96 rounded-xl hover:bg-contain border-2 cursor-pointer"
     >
       <div className="h-9 w-9 rounded-full overflow-hidden border-2 border-amber-50">
         <img src={image} alt="user img"/>
