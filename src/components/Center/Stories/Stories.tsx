340599eff948976048154425d98c46b3e7c9dafc
Ivan <adamovskij00@mail.ru>
2022-03-27 21:55:30 +0300

    add tablet stories list JC

--- src/components/Center/Stories/Stories.tsx
+++ src/components/Center/Stories/Stories.tsx
@@ -3,12 +3,12 @@ import {Story} from "./Story/Story";
 
 export const Stories = () => {
   return (
-    <div className="p-4">
+    <div className="p-4 border">
       <div className="flex items-center justify-between mb-2">
         <h2 className="text-xl font-bold">Stories</h2>
         <a href="/" className='text-blue-400'>See all</a>
       </div>
-      <div className="flex justify-between flex-row mb-5">
+      <div className="flex justify-between tablet:justify-around items-center flex-row mb-5">
         {
           users.map((c, i) => {
             return <Story
