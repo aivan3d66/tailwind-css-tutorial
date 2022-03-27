ddff8d5935c62eb86b51db4b9971bb468829816c
Ivan <adamovskij00@mail.ru>
2022-03-27 22:42:46 +0300

    add content className to CenterContent

--- src/components/Center/CenterContent.tsx
+++ src/components/Center/CenterContent.tsx
@@ -4,9 +4,10 @@ import {Stories} from "./Stories/Stories";
 
 export const CenterContent = () => {
   return (
-    <section className="mobile:w-96 mobile:h-screen mobile:flex mobile:flex-col mobile:justify-center mobile:m-auto tablet:w-1/2 mt-1">
+    <section className="content">
       <Stories/>
       <AddPost/>
+      <Post/>
       <Post/>
     </section>
   )
