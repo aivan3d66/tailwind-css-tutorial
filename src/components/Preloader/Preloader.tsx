3ef91eccb5604c52e6450ea399d4169d13e6ce4c
Ivan <adamovskij00@mail.ru>
2022-04-04 09:24:59 +0300

    add Preloader component

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import preloader from './../../img/preloader.svg'
+
+const Preloader = () => {
+  return (
+    <div>
+      <img className='my-auto w-24 mt-16' src={preloader} alt="Preloader icon"/>
+    </div>
+  )
+}
+
+export default Preloader
